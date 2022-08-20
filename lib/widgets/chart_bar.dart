827cbadc5121baa098e2112cad42ad3292baba06
minhphan46 <21522345@gm.uit.edu.vn>
2022-08-20 18:49:25 +0700

    update appbar

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -11,43 +11,53 @@ class CharBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
-          ),
-        ),
-        SizedBox(height: 4),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
+    // constraints: rang buoc
+    return LayoutBuilder(
+      builder: ((context, constraints) {
+        return Column(
+          children: [
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
               ),
-              FractionallySizedBox(
-                // box co chieu cao ti le voi box khac
-                heightFactor:
-                    spendingPctOfTotal, // phan chieu cao max cho các child
-                child: Container(
+            ),
+            SizedBox(height: constraints.maxHeight * 0.05),
+            Container(
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                children: [
+                  Container(
                     decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(10),
-                )),
+                      border: Border.all(color: Colors.grey, width: 1),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  FractionallySizedBox(
+                    // box co chieu cao ti le voi box khac
+                    heightFactor:
+                        spendingPctOfTotal, // phan chieu cao max cho các child
+                    child: Container(
+                        decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(height: 4),
-        Text(label),
-      ],
+            ),
+            SizedBox(height: constraints.maxHeight * 0.05),
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(label),
+              ),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
